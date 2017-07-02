d67451151feecea5815e210c2cbc8efd3af622a6
sompuis-sopopo <sompuis.sopopo@outlook.com>
2017-07-03 01:47:41 +0800

    Add subscribe and dev tools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,7 +3,6 @@ var redux = require('redux');
 console.log('Starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
-
 	switch (action.type) {
 		case 'CHANGE_NAME':
 			return {
@@ -15,7 +14,17 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
 	}	
 };
 
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+	var state = store.getState();
+
+	console.log('Name is', state.name);
+	document.getElementById('app').innerHTML = state.name;
+});
 
 var currentState = store.getState();
 console.log('currentState', currentState);
@@ -24,4 +33,10 @@ store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Andrew'
 });
-console.log('Name should be andrew', store.getState());
+
+//unsubscribe();
+
+store.dispatch({
+	type: 'CHANGE_NAME',
+	name: 'Emily'
+});
