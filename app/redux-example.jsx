121169858e8ba491c354c94009c6a64fb5bb5e3a
sompuis-sopopo <sompuis.sopopo@outlook.com>
2017-07-03 02:43:32 +0800

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,13 +2,53 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+	name: 'Anonymous',
+	hobbies: [],
+	movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
 	switch (action.type) {
 		case 'CHANGE_NAME':
 			return {
 				...state,
 				name: action.name
 			};
+		case 'ADD_HOBBY':
+			return {
+				...state,
+				hobbies: [
+					...state.hobbies,
+					{
+						id: nextHobbyId++,
+						hobby: action.hobby
+					}
+				]
+			}
+		case 'ADD_MOVIE':
+			return {
+				...state,
+				movies: [
+					...state.movies,
+					{
+						id: nextMovieId++,
+						title: action.title,
+						genre: action.genre
+					}
+				]
+			}
+		case 'REMOVE_HOBBY':
+			return {
+				...state,
+				hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+			}
+		case 'REMOVE_MOVIE':
+			return {
+				...state,
+				movies: state.movies.filter((movie) => movie.id !== action.id)
+			}
 		default:
 			return state;
 	}	
@@ -24,6 +64,8 @@ var unsubscribe = store.subscribe(() => {
 
 	console.log('Name is', state.name);
 	document.getElementById('app').innerHTML = state.name;
+
+	console.log('New state', store.getState());
 });
 
 var currentState = store.getState();
@@ -37,6 +79,38 @@ store.dispatch({
 //unsubscribe();
 
 store.dispatch({
+	type: 'ADD_HOBBY',
+	hobby: 'Running'
+});
+
+store.dispatch({
+	type: 'ADD_HOBBY',
+	hobby: 'Walking'
+});
+
+store.dispatch({
+	type: 'REMOVE_HOBBY',
+	id: 2
+});
+
+store.dispatch({
 	type: 'CHANGE_NAME',
 	name: 'Emily'
+});
+
+store.dispatch({
+	type: 'ADD_MOVIE',
+	title: 'Mad Max',
+	genre: 'Action'
+});
+
+store.dispatch({
+	type: 'ADD_MOVIE',
+	title: 'Star Wars',
+	genre: 'Action'
+});
+
+store.dispatch({
+	type: 'REMOVE_MOVIE',
+	id: 1
 });
